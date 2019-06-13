974126058a5451b7b8d808510656651e35251940
John 'Warthog9' Hawley <jhawley@vmware.com>
2019-06-13 16:34:21 -0700

    Changing the license from Apache-2.0 to Apache-2.0 or GPLv3
    The change is specifically to add an additional license under which the
    code will be licensed (going forward it will be dual licensed),
    specifically to add GPLv3 in addition to it's existing license.  This
    change is being done to better align the code with upstream Ansible.
    
    Signed-off-by: John 'Warthog9' Hawley <jhawley@vmware.com>
    Signed-off-by: John 'Warthog9' Hawley <warthog9@eaglescrag.net>

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,3 +1,6 @@
+# Copyright 2015 VMware, Inc.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 OR GPL-3.0-only
+
 from __future__ import print_function
 import testinfra.utils.ansible_runner
 
